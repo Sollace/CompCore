6368cf6e5a7ef7317752d979f33be8c587f47d2b
warriordog <acomputerdog@gmail.com>
2014-11-29 14:36:45 -0500

    Add Registry and Identifiable

---
+++ source/main/net/acomputerdog/core/identity/Identifiable.java
@@ -0,0 +1,9 @@
+package net.acomputerdog.core.identity;
+
+public interface Identifiable {
+    public String getId();
+
+    public String getDefinition();
+
+    public String getName();
+}
---
+++ source/main/net/acomputerdog/core/storage/Registry.java
@@ -0,0 +1,49 @@
+package net.acomputerdog.core.storage;
+
+import net.acomputerdog.core.identity.Identifiable;
+
+import java.util.*;
+
+public class Registry<T extends Identifiable> {
+    private final Map<String, T> defMap;
+    private final Map<String, T> idMap;
+
+    public Registry() {
+        defMap = new LinkedHashMap<String, T>();
+        idMap = new LinkedHashMap<String, T>();
+    }
+
+    public <T2 extends T> T2 register(T2 item) {
+        defMap.put(item.getDefinition(), item);
+        idMap.put(item.getId(), item);
+        return item;
+    }
+
+    public boolean isDefined(String def) {
+        return defMap.containsKey(def);
+    }
+
+    public boolean isDefined(T item) {
+        return defMap.containsValue(item);
+    }
+
+    public T getFromDef(String def) {
+        return defMap.get(def);
+    }
+
+    public T getFromId(String id) {
+        return idMap.get(id);
+    }
+
+    public Collection<T> getItems() {
+        return defMap.values();
+    }
+
+    public Set<String> getDefinitions() {
+        return Collections.unmodifiableSet(defMap.keySet());
+    }
+
+    public Set<String> getIds() {
+        return Collections.unmodifiableSet(idMap.keySet());
+    }
+}
