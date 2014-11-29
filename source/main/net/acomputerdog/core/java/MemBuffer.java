036bfa200fddb2bcdf2bb9bc60df23728b770c6c
warriordog <acomputerdog@gmail.com>
2014-11-29 15:05:45 -0500

    Make MemBuffer non-static

--- source/main/net/acomputerdog/core/java/MemBuffer.java
+++ source/main/net/acomputerdog/core/java/MemBuffer.java
@@ -3,41 +3,41 @@ package net.acomputerdog.core.java;
 public class MemBuffer {
     private static final int DEFAULT_BUFFER_SIZE = 1000000;
 
-    private static byte[] buffer = null;
+    private byte[] buffer = null;
 
-    public static void createBuffer(int size) {
+    public void createBuffer(int size) {
         if (!isAllocated()) {
             buffer = new byte[size];
         }
     }
 
-    public static void createBuffer() {
+    public void createBuffer() {
         createBuffer(DEFAULT_BUFFER_SIZE);
     }
 
-    public static void destroyBuffer() {
+    public void destroyBuffer() {
         buffer = null;
     }
 
-    public static void freeBuffer() {
+    public void freeBuffer() {
         destroyBuffer();
         System.gc();
     }
 
-    public static void resetBuffer(int size) {
+    public void resetBuffer(int size) {
         freeBuffer();
         createBuffer(size);
     }
 
-    public static void resetBuffer() {
+    public void resetBuffer() {
         resetBuffer(DEFAULT_BUFFER_SIZE);
     }
 
-    public static boolean isAllocated() {
+    public boolean isAllocated() {
         return buffer != null;
     }
 
-    public static int getBufferSize() {
+    public int getBufferSize() {
         return buffer == null ? 0 : buffer.length;
     }
 }
