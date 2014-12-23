4d8949fddb35bb95a26184f42ac8b57cc4eb5ad6
warriordog <acomputerdog@gmail.com>
2014-12-23 18:15:57 -0500

    Add comments to ImmutableLeaf

--- source/main/net/acomputerdog/core/tree/ImmutableLeaf.java
+++ source/main/net/acomputerdog/core/tree/ImmutableLeaf.java
@@ -1,6 +1,16 @@
 package net.acomputerdog.core.tree;
 
+/**
+ * An immutable leaf
+ *
+ * @param <T> The type of this leaf
+ */
 public class ImmutableLeaf<T> extends Leaf<T> {
+    /**
+     * Creates a new ImmutableLeaf
+     * @param branch The branch to attach to
+     * @param item The item this ImmutableLeaf holds
+     */
     public ImmutableLeaf(Branch branch, T item) {
         super(branch, item);
     }
