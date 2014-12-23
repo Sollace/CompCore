36ea883ed83b53861ab4e205aa40b7c57e127c2d
warriordog <acomputerdog@gmail.com>
2014-12-23 11:45:53 -0500

    Deprecate and comment TextFileReader

--- source/main/net/acomputerdog/core/file/TextFileReader.java
+++ source/main/net/acomputerdog/core/file/TextFileReader.java
@@ -7,14 +7,35 @@ import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.List;
 
+@Deprecated //There are better Java-API ways to do this, and this is written badly anyway.
+/**
+ * Class used to easily read a text file into an array of lines
+ */
 public class TextFileReader {
+    /**
+     * The file to read from
+     */
     private final File file;
+    /**
+     * The RandomAccessFile used to read the file.
+     */
     private final RandomAccessFile raf;
 
+    /**
+     * Creates a new TextFileReader
+     *
+     * @param path The path to the file
+     * @throws IOException if IO error occurs.
+     */
     public TextFileReader(String path) throws IOException {
         this(new File(path));
     }
 
+    /**
+     * Creates a new TextFileReader
+     * @param file The file to read from
+     * @throws IOException if IO error occurs.
+     */
     public TextFileReader(File file) throws IOException {
         if (!file.exists()) {
             throw new FileNotFoundException("File does not exist!");
@@ -23,10 +44,19 @@ public class TextFileReader {
         raf = new RandomAccessFile(file, "r");
     }
 
+    /**
+     * Reads the next line in the file
+     * @return Return the next line of text
+     * @throws IOException If IO error occurs.
+     */
     public String readLine() throws IOException {
         return raf.readLine();
     }
 
+    /**
+     * Reads a line safely, without throwing an Exception
+     * @return Return the next line of text, or null if an IO error occurs.
+     */
     public String readLineSafe() {
         try {
             return readLine();
@@ -35,6 +65,12 @@ public class TextFileReader {
         }
     }
 
+    /**
+     * Reads a specified number of lines into an array.
+     * @param amount The number of lines
+     * @return Return an array of lines
+     * @throws IOException if IO error occurs
+     */
     public String[] readLines(int amount) throws IOException {
         List<String> strings = new ArrayList<String>(amount);
         for (int count = 0; count <= amount; count++) {
@@ -47,6 +83,11 @@ public class TextFileReader {
         return strings.toArray(new String[strings.size()]);
     }
 
+    /**
+     * Safely reads a number of lines
+     * @param amount The number of lines to read
+     * @return Return an array of lines.
+     */
     public String[] readLinesSafe(int amount) {
         List<String> strings = new ArrayList<String>(amount);
         for (int count = 0; count <= amount; count++) {
@@ -59,11 +100,20 @@ public class TextFileReader {
         return strings.toArray(new String[strings.size()]);
     }
 
+    /**
+     * Read all lines in the file
+     * @return Return an array of all lines in the file
+     * @throws IOException if IO error occurs
+     */
     public String[] readAllLines() throws IOException {
         raf.seek(0);
         return this.readAllRemainingLines();
     }
 
+    /**
+     * Read all lines in the file safely
+     * @return Return an array of lines
+     */
     public String[] readAllLinesSafe() {
         try {
             raf.seek(0);
@@ -73,6 +123,11 @@ public class TextFileReader {
         return this.readAllRemainingLinesSafe();
     }
 
+    /**
+     * Reads all lines that have not been read
+     * @return Return an array of unread lines
+     * @throws IOException if IO error occurs.
+     */
     public String[] readAllRemainingLines() throws IOException {
         List<String> strings = new ArrayList<String>();
         String str;
@@ -82,6 +137,10 @@ public class TextFileReader {
         return strings.toArray(new String[strings.size()]);
     }
 
+    /**
+     * Safely reads all lines that have not been read
+     * @return Return an array of unread lines
+     */
     public String[] readAllRemainingLinesSafe() {
         List<String> strings = new ArrayList<String>();
         String str;
@@ -91,6 +150,9 @@ public class TextFileReader {
         return strings.toArray(new String[strings.size()]);
     }
 
+    /**
+     * Close the TextFileReader
+     */
     public void close() {
         try {
             raf.close();
@@ -98,14 +160,26 @@ public class TextFileReader {
         }
     }
 
+    /**
+     * Gets the File that this TextFileReader reads from
+     * @return return the File that this TextFileReader reads from
+     */
     public File getFile() {
         return file;
     }
 
+    /**
+     * Get the RandomAccessFile that is reading the file
+     * @return return the RandomAccessFile that is reading the file
+     */
     public RandomAccessFile getRaf() {
         return raf;
     }
 
+    /**
+     * Resets the reader to the beginning of the file
+     * @throws IOException if IO error occurs.
+     */
     public void reset() throws IOException {
         raf.seek(0);
     }
