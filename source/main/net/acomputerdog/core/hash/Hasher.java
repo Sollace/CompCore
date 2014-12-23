62b59b31684275bfb7b1050d154b9cdebdd23d95
warriordog <acomputerdog@gmail.com>
2014-12-23 12:43:14 -0500

    Comment Hasher

--- source/main/net/acomputerdog/core/hash/Hasher.java
+++ source/main/net/acomputerdog/core/hash/Hasher.java
@@ -1,52 +1,110 @@
 package net.acomputerdog.core.hash;
 
+/**
+ * Utility class to assist with calculating HashCodes.
+ * Use like:
+ * public int hashCode() {
+ * return new Hasher().hash(val1).hash(val2).hash(val3).hashCode();
+ * }
+ */
 public class Hasher {
+    /**
+     * The current accumulated value of this Hasher
+     */
     private int currentHashValue;
 
+    /**
+     * Creates a new Hasher
+     */
     public Hasher() {
         currentHashValue = Hash.SEED;
     }
 
+    /**
+     * Hash an Object
+     * @param val the value to hash
+     * @return Return this hasher
+     */
     public Hasher hash(Object val) {
         currentHashValue = Hash.hash(currentHashValue, val);
         return this;
     }
 
+    /**
+     * Hash an int
+     * @param val the value to hash
+     * @return Return this hasher
+     */
     public Hasher hash(int val) {
         currentHashValue = Hash.hash(currentHashValue, val);
         return this;
     }
 
+    /**
+     * Hash a byte
+     * @param val the value to hash
+     * @return Return this hasher
+     */
     public Hasher hash(byte val) {
         currentHashValue = Hash.hash(currentHashValue, val);
         return this;
     }
 
+    /**
+     * Hash a short
+     * @param val the value to hash
+     * @return Return this hasher
+     */
     public Hasher hash(short val) {
         currentHashValue = Hash.hash(currentHashValue, val);
         return this;
     }
 
+    /**
+     * Hash a long
+     * @param val the value to hash
+     * @return Return this hasher
+     */
     public Hasher hash(long val) {
         currentHashValue = Hash.hash(currentHashValue, val);
         return this;
     }
 
+    /**
+     * Hash a boolean
+     * @param val the value to hash
+     * @return Return this hasher
+     */
     public Hasher hash(boolean val) {
         currentHashValue = Hash.hash(currentHashValue, val);
         return this;
     }
 
+    /**
+     * Hash a float
+     * @param val the value to hash
+     * @return Return this hasher
+     */
     public Hasher hash(float val) {
         currentHashValue = Hash.hash(currentHashValue, val);
         return this;
     }
 
+    /**
+     * Hash a double
+     * @param val the value to hash
+     * @return Return this hasher
+     */
     public Hasher hash(double val) {
         currentHashValue = Hash.hash(currentHashValue, val);
         return this;
     }
 
+    /**
+     * Hash a char
+     * @param val the value to hash
+     * @return Return this hasher
+     */
     public Hasher hash(char val) {
         currentHashValue = Hash.hash(currentHashValue, val);
         return this;
