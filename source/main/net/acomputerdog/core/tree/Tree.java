a6e64de8e86ac25c48048b91ad142258ae6eec48
warriordog <acomputerdog@gmail.com>
2014-12-23 18:08:57 -0500

    Add comments to Tree, change root() to getRoot()

--- source/main/net/acomputerdog/core/tree/Tree.java
+++ source/main/net/acomputerdog/core/tree/Tree.java
@@ -1,9 +1,29 @@
 package net.acomputerdog.core.tree;
 
+/**
+ * A tree-modeled data structure
+ *
+ * @param <T> The type of the data
+ */
 public class Tree<T> {
-    private final Branch<T> root = new Branch<T>(this, null);
+    /**
+     * Root branch of this Tree
+     */
+    private final Branch<T> root;
 
-    public Branch<T> root() {
+    /**
+     * Creates a new Tree
+     */
+    public Tree() {
+        root = new Branch<T>(this, null);
+    }
+
+    /**
+     * Gets the root branch of this Tree
+     *
+     * @return return the root branch of this tree
+     */
+    public Branch<T> getRoot() {
         return root;
     }
 }
