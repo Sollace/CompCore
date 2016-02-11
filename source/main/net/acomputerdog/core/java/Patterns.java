4330d1b3a2d4195da1a55955771914fb97757e36
Sollace <sollacea@gmail.com>
2016-02-11 14:56:06 +0200

    * Quoted backslash

--- source/main/net/acomputerdog/core/java/Patterns.java
+++ source/main/net/acomputerdog/core/java/Patterns.java
@@ -92,7 +92,7 @@ public class Patterns {
     public static final String OPEN_CURLY_BRACKETS = quote("{");
     public static final String CLOSE_CURLY_BRACKETS = quote("}");
     public static final String FORWARD_SLASH = "/";
-    public static final String BACK_SLASH = "\\";
+    public static final String BACK_SLASH = quote("\\");
 
     public static final String SECTONSIGN = "\u0167";
     
