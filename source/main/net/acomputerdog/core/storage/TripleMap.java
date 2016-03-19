bb2fbdde43292f07641e04737687f0f4221ba53f
Sollace <sollacea@gmail.com>
2016-03-19 15:04:37 +0200

    Replaced TripleStringMap with a generic version

--- source/main/net/acomputerdog/core/storage/TripleMap.java
+++ source/main/net/acomputerdog/core/storage/TripleMap.java
@@ -6,74 +6,74 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Maps 3 strings together so that the entire set can be retrieved by using any of the strings as a key.
+ * Maps 3 object together so that the entire set can be retrieved by using any of the them as a key.
  */
-public class TripleStringMap {
+public class TripleMap<T, K, U> {
 
-    private final Map<String, TSMItem> item1Map = new HashMap<String, TSMItem>();
-    private final Map<String, TSMItem> item2Map = new HashMap<String, TSMItem>();
-    private final Map<String, TSMItem> item3Map = new HashMap<String, TSMItem>();
+    private final Map<T, TSMItem> item1Map = new HashMap<T, TSMItem>();
+    private final Map<K, TSMItem> item2Map = new HashMap<K, TSMItem>();
+    private final Map<U, TSMItem> item3Map = new HashMap<U, TSMItem>();
 
-    public void addItems(String string1, String string2, String string3) {
+    public void addItems(T string1, K string2, U string3) {
         TSMItem items = new TSMItem(string1, string2, string3);
         putItem(item1Map, string1, items);
         putItem(item2Map, string2, items);
         putItem(item3Map, string3, items);
     }
 
-    private void putItem(Map<String, TSMItem> cat, String item, TSMItem items) {
-        if (item != null && !item.trim().isEmpty()) {
+    private <V> void putItem(Map<V, TSMItem> cat, V item, TSMItem items) {
+        if (item != null && (item instanceof String && !((String)item).trim().isEmpty())) {
             cat.put(item, items);
         }
     }
 
-    public TSMItem getFrom1(String item) {
+    public TSMItem getFrom1(T item) {
         return item1Map.get(item);
     }
 
-    public TSMItem getFrom2(String item) {
+    public TSMItem getFrom2(K item) {
         return item2Map.get(item);
     }
 
-    public TSMItem getFrom3(String item) {
+    public TSMItem getFrom3(U item) {
         return item3Map.get(item);
     }
 
-    public boolean hasItem1(String item1) {
+    public boolean hasItem1(T item1) {
         return item1Map.containsKey(item1);
     }
 
-    public boolean hasItem2(String item2) {
+    public boolean hasItem2(K item2) {
         return item2Map.containsKey(item2);
     }
 
-    public boolean hasItem3(String item3) {
+    public boolean hasItem3(U item3) {
         return item3Map.containsKey(item3);
     }
 
     /**
      * A binding of three strings
      */
-    public static class TSMItem {
-        private final String item1;
-        private final String item2;
-        private final String item3;
+    public class TSMItem {
+        private final T item1;
+        private final K item2;
+        private final U item3;
 
-        private TSMItem(String item1, String item2, String item3) {
+        private TSMItem(T item1, K item2, U item3) {
             this.item1 = item1;
             this.item2 = item2;
             this.item3 = item3;
         }
 
-        public String getItem1() {
+        public T getItem1() {
             return item1;
         }
 
-        public String getItem2() {
+        public K getItem2() {
             return item2;
         }
 
-        public String getItem3() {
+        public U getItem3() {
             return item3;
         }
 
@@ -90,9 +90,9 @@ public class TripleStringMap {
         public boolean equals(Object obj) {
             if (obj == null) return false;
             if (obj == this) return true;
-            if (obj instanceof TSMItem) {
+            if (obj instanceof TripleMap.TSMItem) {
                 TSMItem other = (TSMItem) obj;
-                return doesEqual(other.item1, this.item1) && doesEqual(other.item2, this.item2) && doesEqual(other.item3, this.item3);
+                return doesEqual(other.item1, item1) && doesEqual(other.item2, item2) && doesEqual(other.item3, item3);
             }
             return false;
         }
